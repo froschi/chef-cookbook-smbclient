9f4faeee781b573d0e098169f536bb1228474a1e
Thorsten Fischer <thorsten@froschi.org>
2012-06-12 17:59:10 +0200

    Added dependencies.
    Signed-off-by: Thorsten Fischer <thorsten@froschi.org>

--- metadata.rb
+++ metadata.rb
@@ -6,3 +6,17 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
 %w/ubuntu/.each { |os| supports os }
+
+depends          "libcap", ">= 0.0.1"
+depends          "libcomerr", ">= 0.0.1"
+depends          "libgssapi-krb5", ">= 0.0.1"
+depends          "libk5crypto", ">= 0.0.1"
+depends          "libkrb5", ">= 0.0.1"
+depends          "libldap", ">= 0.0.1"
+depends          "libncurses", ">= 0.0.1"
+depends          "libpopt", ">= 0.0.1"
+depends          "libreadline", ">= 0.0.1"
+depends          "libtalloc", ">= 0.0.1"
+depends          "libtdb", ">= 0.0.1"
+depends          "libwbclient", ">= 0.0.1"
+depends          "samba-common", ">= 0.0.1"
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,11 +1,29 @@
+include_recipe "libcap"
+include_recipe "libcomerr"
+include_recipe "libgssapi-krb5"
+include_recipe "libk5crypto"
+include_recipe "libkrb5"
+include_recipe "libldap"
+include_recipe "libncurses"
+include_recipe "libpopt"
+include_recipe "libreadline"
+include_recipe "libtalloc"
+include_recipe "libtdb"
+include_recipe "libwbclient"
+include_recipe "samba-common"
+
 packages = Array.new
 
 case node[:lsb][:codename]
 when "lucid"
+  include_recipe "libncurses"
+
   packages |= %w/
     smbclient
   /
 when "precise"
+  include_recipe "libtdb"
+
   packages |= %w/
     smbclient
   /
